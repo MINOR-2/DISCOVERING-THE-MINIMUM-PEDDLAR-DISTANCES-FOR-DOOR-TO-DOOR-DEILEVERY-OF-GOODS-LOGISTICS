dc23b1109a21362017443f9215b5ffc1213a7c24
Raghav Kapoor <raghavkapoor@Raghavs-MacBook-Air.local>
2021-03-17 18:34:52 +0530

    Added the logic to calculate the fitness value of individual city traversal order set

--- MINOR.java
+++ MINOR.java
@@ -3,8 +3,8 @@ class Peddlar
 {
     Scanner sc=new Scanner(System.in);
     Random rant=new Random();
-    
-    
+    int dis[][]=new int[5][5];
+
     void initialiseCityOrder(int n)
     {
         int arr[][]=new int[n][n];
@@ -13,54 +13,67 @@ class Peddlar
         {
             l1.add(i);
         }
-        for(int i=0;i<n;i++)
+        for(int i=0;i<5;i++)
         {
         
         Collections.shuffle(l1);
-        for(int j=0;j<n;j++)
+        for(int j=0;j<5;j++)
         {
-            arr[i][j]=l1.get(j)+1;
+            arr[i][j]=l1.get(j);
         }
         }
         System.out.println();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<5;i++)
         {
-            for(int j=0;j<n;j++)
+            int fit_value=0;
+
+            System.out.println("CITY VISITING PATTERN NO-"+(i+1)+" ");
+            System.out.print(" | ");
+            for(int j=0;j<5;j++)
             {
-                System.out.print(arr[i][j]+" ");
+                System.out.print(arr[i][j]+" | ");
             }
+            for(int j=0;j+1<5;j++)
+            {
+                fit_value+=dis[arr[i][j]][arr[i][j+1]];
+            }
+            fit_value=fit_value+dis[arr[i][4]] [arr[i][0]];
+            System.out.println();
+            System.out.println("FIT_VALUE="+fit_value);
+            System.out.println();
             System.out.println();
         }
     }
     void initialiseCityDistance(int n)
     {
-        int arr[][]=new int[n][n];
         for(int i=0;i<5;i++)
         {
             for(int j=i+1;j<5;j++)
             {
                 if(i==j)
                 {
-                    arr[i][j]=0;
+                    dis[i][j]=0;
                     continue;
                 }
-                arr[i][j]=rant.nextInt(9)+1;
-                arr[j][i]=arr[i][j];
+                dis[i][j]=rant.nextInt(9)+1;
+                dis[j][i]=dis[i][j];
             }
-            
         }
-        
-        
-        for(int i=0;i<5;i++)
+        System.out.println("DISTANCE OF ONE CITY TO ANOTHER CITY");
+            for(int i=0;i<5;i++)
         {
             for(int j=0;j<5;j++)
             {
-                System.out.print(arr[i][j]+" ");
+                System.out.print(dis[i][j]+" ");
             }
             System.out.println();
             
-            
         }
+            
+        
+        
+        
+        
         
     }
 }
@@ -71,4 +84,5 @@ public class MyClass {
      P1.initialiseCityDistance(5);
      P1.initialiseCityOrder(5);
     }
+    
 }
