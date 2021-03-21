34169eef8cedc11e6f7ac0f955d37c754aadc232
Anshul Chaudhary <60177875+anshulgit7@users.noreply.github.com>
2021-03-21 23:33:52 +0530

    Selection done

--- MINOR.java
+++ MINOR.java
@@ -51,7 +51,7 @@ class Peddlar
         {
             System.out.print(+fit_value_sequence[i]+" ");
         }
-        initialisePickfitnessValue(fit_value_sequence);
+        initialisePickfitnessValue(fit_value_sequence,arr);
 
     }
     void initialiseCityDistance(int n)
@@ -86,10 +86,29 @@ class Peddlar
         
         
     }
-    void initialisePickfitnessValue(int[] fit_value_sequence)
+    void initialisePickfitnessValue(int[] fit_value_sequence,int[][] arr)
     {
     int m = fit_value_sequence.length;
-		 
+	int[][] a = new int[m][m+1];
+	
+	for(int i=0;i<m;i++)
+	    for(int j=0;j<m;j++)
+	        a[i][j] = arr[i][j];
+        
+        for(int i=0;i<5;i++)
+        for(int j=5;j<6;j++)
+            a[i][j] = fit_value_sequence[i];
+        
+        int ar[][] = new int[m][m+1];
+        ar = sortbyColumn(a,m);
+        /*System.out.println();
+        System.out.println("print");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<m+1;j++)
+                System.out.print(ar[i][j]+" ");
+            System.out.println();
+        }*/
 		for(int i = 0; i< m-1; i++) {
 			
 			for (int j= 0; j<m-1-i; j++) {
@@ -104,14 +123,59 @@ class Peddlar
 			}
 		}
 		System.out.println();
-		System.out.println("Aftersorting the fitness_sequence on the basics of fitness:");
+		System.out.println("After sorting the fitness_sequence on the basics of fitness:");
 		for (int item: fit_value_sequence) {
 			System.out.print(item+" ");
 		}
+		selection(ar,m);
     }
+    static int[][] sortbyColumn(int arr[][], int col)
+        {
+            Arrays.sort(arr, new Comparator<int[]>() {
+
+            public int compare(final int[] entry1,
+            final int[] entry2) {
+
+            if (entry1[col] < entry2[col])
+            return 1;
+            else
+            return -1;
+        }
+        }); 
+
+        return arr;
+        }
+    static void selection (int[][] arr,int m1)
+    {
+        int m = m1;
+        int[] selected1 = new int[m+1];
+        int[] selected2 = new int[m+1];
+        for(int i=0;i<1;i++)
+            for(int j=0;j<m+1;j++)
+                selected1[j] = arr[i][j];
+                
+        for(int i=1;i<2;i++)
+            for(int j=0;j<m+1;j++)
+                selected2[j] = arr[i][j];
+        
+        System.out.println();
+        System.out.println();
+        System.out.println("The two paths which are selected for crossover are - ");
+        
+            System.out.println("Path - 1");
+            for(int j=0;j<m+1;j++)
+                System.out.print(selected1[j]+" ");
+            System.out.println();
+            System.out.println("Path - 2");
+            for(int j=0;j<m+1;j++)
+                System.out.print(selected2[j]+" ");
+            System.out.println();
+            
+    }
+    
 }
 
-public class MyClass {
+public class Main {
     public static void main(String args[]) 
     {
      Peddlar P1=new Peddlar();
